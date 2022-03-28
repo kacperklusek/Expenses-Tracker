075df8991bafe89b6a77153838bdf0bc13f1e9dd
Kacper <kacperklusek70@gmail.com>
2022-03-28 13:16:18 +0200

    fetching transactions from mongodb

--- frontend/src/context/contextReducer.js
+++ frontend/src/context/contextReducer.js
@@ -3,14 +3,21 @@
 const contextReducer = (state, action) => {
   let transactions
   switch (action.type) {
+    case "FETCH_SUCCESS":
+      console.log(JSON.stringify(action.payload));
+      // return []
+      return action.payload
+
     case "DELETE_TRANSACTION":
       transactions = state.filter((t) => t.id !== action.payload);
       localStorage.setItem('transactions', JSON.stringify(transactions))
+      // TODO tutaj zaktualizować w mongo itemsy
 
       return transactions
     case "ADD_TRANSACTION":
       transactions = [action.payload, ...state]
       localStorage.setItem('transactions', JSON.stringify(transactions))
+      // TODO tutaj zaktualizować w mongo itemsy
 
       return transactions
     default:
