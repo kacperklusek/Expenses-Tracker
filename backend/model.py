04ccee1f408bd5be07d380b969d0a66822725297
Karol Wrona <karol.wrona01@gmail.com>
2022-03-30 08:44:38 +0200

    started on adding categories

--- backend/model.py
+++ backend/model.py
@@ -2,6 +2,7 @@
 #  FARMSTACK Tutorial - Sunday 13.06.2021
 
 # Pydantic allows auto creation of JSON Schemas from models
+from xmlrpc.client import boolean
 from pydantic import BaseModel
 
 
@@ -11,3 +12,14 @@ class Transaction(BaseModel):
   type: str
   date: str
   id: str
+
+# dobrze by było dodać do bazy kategorie
+# i potem zamiast categories z constants
+# pobierać dane z bazy danych, wtedy użytkownik może dodawać własne kategorie
+# Dobrze by też było ustawić coś w stylu klucza głównego?
+# żeby nie było można dodawać kategori o tej samej nazwie
+# Postaram się to ogarnąć w wolnej chwili
+# class Category(BaseModel):
+#   name: str
+#   color: str
+#   user: boolean
